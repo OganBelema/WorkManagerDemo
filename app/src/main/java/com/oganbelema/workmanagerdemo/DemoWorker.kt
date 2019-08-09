11d39bbb1330942a71c76e4361ecc0a8f6e34adf
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-09 09:06:53 +0100

    Create worker implementation

--- app/src/main/java/com/oganbelema/workmanagerdemo/DemoWorker.kt
+++ app/src/main/java/com/oganbelema/workmanagerdemo/DemoWorker.kt
@@ -1,7 +1,26 @@
 package com.oganbelema.workmanagerdemo
 
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
 /**
  * Created by Belema Ogan on 2019-08-09.
  */
-class DemoWorker {
+class DemoWorker(private val context: Context, private val workerParameters: WorkerParameters):
+    Worker(context, workerParameters) {
+
+    private val tag = this::class.simpleName
+
+
+    override fun doWork(): Result {
+        for (i in 1..1000){
+            Log.i(tag, "Count is $i")
+        }
+
+        return Result.success()
+    }
+
+
 }
