8ae036e21993de5c0bdde65731ad48532df7690e
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-09 09:09:17 +0100

    Add floating button and start work with button click

--- app/src/main/java/com/oganbelema/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/oganbelema/workmanagerdemo/MainActivity.kt
@@ -2,11 +2,20 @@ package com.oganbelema.workmanagerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(DemoWorker::class.java).build()
+
+        floatingActionButton.setOnClickListener {
+            WorkManager.getInstance(this).enqueue(oneTimeWorkRequest)
+        }
     }
 }
